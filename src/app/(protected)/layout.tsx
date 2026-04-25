3643d589835a0d309df7de330be1fc7e8cf8d4b4
Dawit Negash <dawitnegash02@gmail.com>
2026-04-26 00:40:38 +0300

    style: add global padding to protected layout

--- src/app/(protected)/layout.tsx
+++ src/app/(protected)/layout.tsx
@@ -17,7 +17,9 @@ export default async function ProtectedLayout({
 
   return (
     <div className="min-h-screen bg-neutral-50 flex flex-col">
-      {children}
+      <main className="flex-1 w-full max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-8 md:py-12">
+        {children}
+      </main>
     </div>
   )
 }
